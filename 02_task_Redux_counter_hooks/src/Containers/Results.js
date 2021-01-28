16cff8703c1bce0af4f29e3e76eacb12655ad2fa
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-01-28 16:12:39 +0200

    results styling

--- 02_task_Redux_counter_hooks/src/Containers/Results.js
+++ 02_task_Redux_counter_hooks/src/Containers/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { storeResult, removeResult } from '../actions/actions';
+import './results.css'
 
 
 const Results = () => {
